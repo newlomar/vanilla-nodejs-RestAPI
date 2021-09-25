137277d9fd3a97be0920d8ae2e75699d36934c4d
Newton Lomar <newtonlomar16@gmail.com>
2021-09-25 10:43:32 -0300

    send a simple response

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http')
+const products = require('./data/products')
 
 const server = http.createServer((req, res) => {
-  console.log(123)
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(products))
 })
 
 const PORT = process.env.PORT || 5000
