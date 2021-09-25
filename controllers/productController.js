5dfaf58b9d386b70e5a13f4ae51142dcdc69fb8d
Newton Lomar <newtonlomar16@gmail.com>
2021-09-25 12:45:54 -0300

    updating product

--- controllers/productController.js
+++ controllers/productController.js
@@ -65,8 +65,47 @@ async function createProduct(req, res) {
 
 }
 
+// @desc UPDATE a Product
+// @route PUT /api/products/:id
+async function updateProduct(req, res, id) {
+
+  try {
+
+    const product = await Product.findById(id)
+
+    if (!product) {
+      res.writeHead(404, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ message: 'Product Not Found' }))
+    }
+
+    else {
+
+      const body = await getPostData(req)
+
+      const { title, description, price } = JSON.parse(body)
+
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price
+      }
+
+      const updatedProduct = await Product.update(id, productData)
+
+      res.writeHead(200, { 'Content-Type': 'application/json' })
+      return res.end(JSON.stringify(updatedProduct))
+
+    }
+
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
 module.exports = {
   getProducts,
   getProduct,
-  createProduct
+  createProduct,
+  updateProduct
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getProducts, getProduct, createProduct } = require('./controllers/productController')
+const { getProducts, getProduct, createProduct, updateProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
   
@@ -8,17 +8,27 @@ const server = http.createServer((req, res) => {
     getProducts(req, res)
 
   }
+
   else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
 
     const id = req.url.split('/')[3]
     getProduct(req, res, id)
 
   }
+
   else if (req.url === '/api/products' && req.method === 'POST') {
 
     createProduct(req,res)
 
   }
+
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+
+    const id = req.url.split('/')[3]
+    updateProduct(req, res, id)
+
+  }
+
   else {
 
     res.writeHead(404, { 'Content-Type': 'application/json' })
