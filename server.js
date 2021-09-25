478fe6a7fef8952878bfebd67c74d40e58e4a3df
Newton Lomar <newtonlomar16@gmail.com>
2021-09-25 12:55:04 -0300

    deleting product

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getProducts, getProduct, createProduct, updateProduct } = require('./controllers/productController')
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
 
 const server = http.createServer((req, res) => {
   
@@ -26,6 +26,13 @@ const server = http.createServer((req, res) => {
 
     const id = req.url.split('/')[3]
     updateProduct(req, res, id)
+
+  }
+
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+
+    const id = req.url.split('/')[3]
+    deleteProduct(req, res, id)
 
   }
 
